47b42c760446f32efd202391f3e2f258d1e9ef4f
NgoNamKhanh-alt <ngonamkhanh2710@gmail.com>
2025-03-04 08:10:58 +0700

    Initial commit

---
+++ Project2/FileName.cpp
@@ -0,0 +1,11 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+int main(int argc, char** argv) {
+	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_PNG);
+	TTF_Init();
+	return 0;
+
+}
