056abdc7d7109d7f10bc1a80774a596c8968ddcb
NgoNamKhanh-alt <ngonamkhanh2710@gmail.com>
2025-03-20 22:19:14 +0700

    Quick game changes and created bullet

--- Project2/FileName.cpp
+++ Project2/FileName.cpp
@@ -3,11 +3,13 @@
 #include <SDL_ttf.h>
 #include <iostream>
 #include <vector>
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-const int TILE_SIZE = 40;
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+const int TILE_SIZE = 36;
 const int MAP_WIDTH = SCREEN_WIDTH / TILE_SIZE;
 const int MAP_HEIGHT = SCREEN_HEIGHT / TILE_SIZE;
+const int SHOT_DELAY = 250;
+int last_shot_time = 0;
 class Wall {
 public:
 	int x, y;
@@ -23,33 +25,53 @@ public:
 		if (active) {
 			SDL_SetRenderDrawColor(renderer, 150, 75, 0, 255);
 			SDL_RenderFillRect(renderer, &rect);
-
 		}
 	}
 };
-class PlayerTank {
+class Player_Bullet {
+public:
+	int x, y;
+	int speed = -9;
+	int dirX, dirY;
+	SDL_Rect rect;
+	Player_Bullet(int startX, int startY) {
+		x = startX;
+		y = startY;
+		rect = { x, y, TILE_SIZE/3, TILE_SIZE/3};
+		dirX = 0;
+		dirY = -1;
+	}
+	void move() {
+		int new_Y = y + speed;
+			y = new_Y;
+			rect.x = x;
+			rect.y = y;
+		
+	}
+	void render(SDL_Renderer* renderer) {
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderFillRect(renderer, &rect);
+	}
+};
+class Player_Tank {
 public:
 	int x, y;
 	int dirX, dirY;
 	SDL_Rect rect;
-	PlayerTank() : x(0), y(0), dirX(0), dirY(0){}
-	PlayerTank(int startX, int startY){
+	Player_Tank() : x(0), y(0), dirX(0), dirY(0){ rect = { x, y, TILE_SIZE, TILE_SIZE }; }
+	Player_Tank(int startX, int startY){
 		x = startX;
 		y = startY;
+		rect = { x, y, TILE_SIZE, TILE_SIZE };
 		dirX = 0;
 		dirY = -1;
 	}
-	void move(int dx, int dy, const std::vector<Wall>& walls) {
+
+	void move(int dx, int dy) {
 		int newX = x + dx;
 		int newY = y + dy;
 		this->dirX = dx;
 		this->dirY = dy;
-		SDL_Rect newRect = { newX, newY, TILE_SIZE, TILE_SIZE };
-		for (int i = 0; i < walls.size(); ++i) {
-			if (SDL_HasIntersection(&newRect, &walls[i].rect)) {
-				return;
-			}
-		}
 		if (newX >= TILE_SIZE && newX <= SCREEN_WIDTH - TILE_SIZE * 2 && newY >= TILE_SIZE && newY <= SCREEN_HEIGHT - TILE_SIZE * 2) {
 			x = newX;
 			y = newY;
@@ -65,10 +87,11 @@ public:
 };
 class Game {
 public: 
-	PlayerTank player;
+	Player_Tank player;
 	std::vector<Wall> walls;
 	SDL_Window* window;
 	SDL_Renderer* renderer;
+	std::vector<Player_Bullet> bullets;
 	bool running;
 	Game() {
 		running = true;
@@ -76,25 +99,24 @@ public:
 		{
 			std::cerr << "SDL could not initialize! SDL Error:" << SDL_GetError() << "\n";
 			running = false;
-			window = SDL_CreateWindow("Battle City", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		}
+		window = SDL_CreateWindow("Battle City", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if (!window) {
 			std::cerr << "Window could not be created! SDL Error: " << SDL_GetError() << "\n";
 			running = false;
-			renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 		}
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 		if (!renderer) {
 			std::cerr << "Renderer could not be created! SDL Error: " << SDL_GetError() << "\n";
 			running = false;
 		}
-		generateWalls(); 
-		player = PlayerTank((((MAP_WIDTH - 1) / 2) * TILE_SIZE), ((MAP_HEIGHT - 2) * TILE_SIZE));
+		player = Player_Tank((((MAP_WIDTH - 1) / 2) * TILE_SIZE), ((MAP_HEIGHT - 2) * TILE_SIZE));
 	}
-	void generateWalls() {
-		for (int i{ 3 }; i < MAP_HEIGHT - 3; i += 2) {
-			for (int j{ 3 }; j < MAP_WIDTH - 3; j += 2) {
-				Wall w = Wall(j * TILE_SIZE, i * TILE_SIZE);
-				walls.push_back(w);
+	void update() {
+		for (int i = 0; i < bullets.size(); ++i) {
+			bullets[i].move();
+			if (bullets[i].y < 0) {
+				bullets.erase(bullets.begin() + i);
 			}
 		}
 	}
@@ -102,17 +124,23 @@ public:
 		SDL_SetRenderDrawColor(renderer, 128,128,128,255);
 		SDL_RenderClear(renderer);
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		for (int i = 1; i <= MAP_HEIGHT; ++i) {
-			for (int j = 1; j <= MAP_WIDTH; ++j) {
-				SDL_Rect rect = { i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE };
+		for (int i = 0; i < MAP_HEIGHT; ++i) {
+			for (int j = 0; j < MAP_WIDTH; ++j) {
+				SDL_Rect rect = { j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE };
 				SDL_RenderFillRect(renderer, &rect);
 			}
+		}
+		player.render(renderer);
+		for (int i = 0; i < bullets.size(); ++i) {
+			bullets[i].render(renderer);
 		}
 		SDL_RenderPresent(renderer);
 	}
 	void run() {
 		while (running) {
+			handleEvents();
 			render();
+			update();
 			SDL_Delay(16);
 		}
 	}
@@ -121,17 +149,25 @@ public:
 		const Uint8* keystates = SDL_GetKeyboardState(NULL);
 
 		if (keystates[SDL_SCANCODE_W]) {
-			player.move(0, -5, walls);  // Move up
+			player.move(0, -5);  // Move up
 		}
 		if (keystates[SDL_SCANCODE_S]) {
-			player.move(0, 5, walls);   // Move down
+			player.move(0, 5);   // Move down
 		}
 		if (keystates[SDL_SCANCODE_A]) {
-			player.move(-5, 0, walls);  // Move left
+			player.move(-5, 0);  // Move left
 		}
 		if (keystates[SDL_SCANCODE_D]) {
-			player.move(5, 0, walls);   // Move right
+			player.move(5, 0);   // Move right
 		}
+		Uint32 currentTime = SDL_GetTicks();
+		if (keystates[SDL_SCANCODE_SPACE]) {
+			if (currentTime - last_shot_time >= SHOT_DELAY) {
+				bullets.push_back(Player_Bullet(player.x + TILE_SIZE / 3, player.y + TILE_SIZE / 3 - TILE_SIZE + 4));
+				last_shot_time = currentTime;
+			}
+		}
+		
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT) {
 				running = false;
@@ -149,7 +185,6 @@ int main(int argc, char** argv) {
 	Game game;
 	if (game.running) {
 		game.run();
-		game.handleEvents();
 		SDL_Delay(16);
 	}
 	return 0;
