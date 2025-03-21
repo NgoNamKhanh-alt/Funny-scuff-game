1c280042bad972a9c40a72e1056f132a48039d68
NgoNamKhanh-alt <ngonamkhanh2710@gmail.com>
2025-03-21 21:44:02 +0700

    Make more bullets

--- Project2/FileName.cpp
+++ Project2/FileName.cpp
@@ -3,52 +3,64 @@
 #include <SDL_ttf.h>
 #include <iostream>
 #include <vector>
+#include <random>
+#include <chrono>
 const int SCREEN_WIDTH = 1260;
 const int SCREEN_HEIGHT = 720;
 const int TILE_SIZE = 36;
 const int MAP_WIDTH = SCREEN_WIDTH / TILE_SIZE;
 const int MAP_HEIGHT = SCREEN_HEIGHT / TILE_SIZE;
 const int SHOT_DELAY = 250;
-const int ENEMY_SHOT_DELAY = 500;
+const int ENEMY_SHOT_DELAY = 1500;
+const int ENEMY_SPEED_DELAY = 2500;
 int last_shot_time = 0;
 int last_enemy_shot_time = 0;
+int last_speed_change = 0;
+std::mt19937 mt{ static_cast<std::mt19937::result_type>(
+	std::chrono::steady_clock::now().time_since_epoch().count()
+	) };
+std::uniform_int_distribution<> speed_enemy{ -2, 2 };
+
 class Enemy_Bullet {
 public:
 	int x, y;
+	double dirX, dirY;
 	int damage = 1;
-	int speed = 7;
-	int dirX, dirY;
+	double speed = 6.25;
 	SDL_Rect rect;
-	Enemy_Bullet(int startX, int startY) {
+	Enemy_Bullet(int startX, int startY, double dirX, double dirY) {
 		x = startX;
 		y = startY;
+		this->dirX = dirX;
+		this->dirY = dirY;
 		rect = { x, y, TILE_SIZE / 2, TILE_SIZE / 2 };
-		dirX = 0;
-		dirY = -1;
 	}
 	void move() {
-		int new_Y = y + speed;
-		y = new_Y;
+		x = x + (speed * dirX);
+		y = y + (speed * dirY);
 		rect.x = x;
 		rect.y = y;
 	}
 	void render(SDL_Renderer* renderer) {
-		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
 		SDL_RenderFillRect(renderer, &rect);
 	}
 };
 class Enemy {
 public:
 	int x, y;
+	int y_speed = 1;
 	SDL_Rect rect;
 	Enemy(int startX, int startY) {
 		x = startX;
 		y = startY;
 		rect = { x, y, TILE_SIZE * 3, TILE_SIZE * 3 };
 	}
-	void move(int x_speed) {
+	void move(int x_speed, int y_speed) {
 		x = x + x_speed;
+		y = y + y_speed;
 		rect.x = x;
+		rect.y = y;
 	}
 	void render(SDL_Renderer* renderer) {
 		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
@@ -65,7 +77,7 @@ public:
 	Player_Bullet(int startX, int startY) {
 		x = startX;
 		y = startY;
-		rect = { x, y, TILE_SIZE/3, TILE_SIZE/3};
+		rect = { x, y, TILE_SIZE/3, TILE_SIZE/3 };
 		dirX = 0;
 		dirY = -1;
 	}
@@ -80,13 +92,13 @@ public:
 		SDL_RenderFillRect(renderer, &rect);
 	}
 };
-class Player_Tank {
+class Player {
 public:
 	int x, y;
 	int dirX, dirY;
 	SDL_Rect rect;
-	Player_Tank() : x(0), y(0), dirX(0), dirY(0){ rect = { x, y, TILE_SIZE, TILE_SIZE }; }
-	Player_Tank(int startX, int startY){
+	Player() : x(0), y(0), dirX(0), dirY(0){ rect = { x, y, TILE_SIZE, TILE_SIZE }; }
+	Player(int startX, int startY){
 		x = startX;
 		y = startY;
 		rect = { x, y, TILE_SIZE, TILE_SIZE };
@@ -115,7 +127,7 @@ public:
 
 class Game {
 public: 
-	Player_Tank player;
+	Player player;
 	SDL_Window* window;
 	SDL_Renderer* renderer;
 	std::vector<Player_Bullet> bullets;
@@ -139,7 +151,7 @@ public:
 			std::cerr << "Renderer could not be created! SDL Error: " << SDL_GetError() << "\n";
 			running = false;
 		}
-		player = Player_Tank((((MAP_WIDTH - 1) / 2) * TILE_SIZE), ((MAP_HEIGHT - 2) * TILE_SIZE));
+		player = Player((((MAP_WIDTH - 1) / 2) * TILE_SIZE), ((MAP_HEIGHT - 2) * TILE_SIZE));
 	}
 	void update() {
 		for (int i = 0; i < bullets.size(); ++i) {
@@ -150,15 +162,38 @@ public:
 		}
 		int enemy_center = enemy.x + enemy.rect.w / 2;
 		int player_center = player.x + player.rect.w / 2;
+		Uint32 current_speed_change = SDL_GetTicks();
+		if (enemy.y < 20) {
+			enemy.y_speed = 2;
+			last_speed_change = current_speed_change;
+		}
+		else if (enemy.y > 315) {
+			enemy.y_speed = -2;
+			last_speed_change = current_speed_change;
+		}
+		else if (current_speed_change - last_speed_change >= ENEMY_SPEED_DELAY) {
+			enemy.y_speed = speed_enemy(mt);
+			if (enemy.y_speed == 0) {
+				enemy.y_speed = 1;
+			}
+			last_speed_change = current_speed_change;
+		}
 		if (enemy_center > player_center) {
-			enemy.move(-3);
+			enemy.move(-3,enemy.y_speed);
 		}
 		else if (enemy_center < player_center) {
-			enemy.move(3);
+			enemy.move(3,enemy.y_speed);
 		}
 		Uint32 enemy_currentTime = SDL_GetTicks();
 		if (enemy_currentTime - last_enemy_shot_time >= ENEMY_SHOT_DELAY) {
-			enemy_bullets.push_back(Enemy_Bullet(enemy_center, enemy.y + TILE_SIZE * 3 + TILE_SIZE + 4));
+			enemy_bullets.push_back(Enemy_Bullet(enemy_center, enemy.y + TILE_SIZE * 3, 0, 1));
+			enemy_bullets.push_back(Enemy_Bullet(enemy_center + TILE_SIZE, enemy.y + TILE_SIZE * 3, 0.75, 0.85));
+			enemy_bullets.push_back(Enemy_Bullet(enemy_center - TILE_SIZE, enemy.y + TILE_SIZE * 3, -0.75, 0.85));
+			enemy_bullets.push_back(Enemy_Bullet(enemy_center - TILE_SIZE * 2, enemy.y + TILE_SIZE , -1, 0));
+			enemy_bullets.push_back(Enemy_Bullet(enemy_center + TILE_SIZE * 2, enemy.y + TILE_SIZE, 1, 0));
+			enemy_bullets.push_back(Enemy_Bullet(enemy_center - TILE_SIZE, enemy.y - TILE_SIZE, -0.75, -0.85));
+			enemy_bullets.push_back(Enemy_Bullet(enemy_center + TILE_SIZE, enemy.y - TILE_SIZE, 0.75, -0.85));
+			enemy_bullets.push_back(Enemy_Bullet(enemy_center, enemy.y - TILE_SIZE, 0, -1));
 			last_enemy_shot_time = enemy_currentTime;
 		}
 		for (int i = 0; i < enemy_bullets.size(); ++i) {
@@ -167,6 +202,7 @@ public:
 				enemy_bullets.erase(enemy_bullets.begin() + i);
 			}
 		}
+
 	}
 	void render() {
 	
@@ -174,7 +210,7 @@ public:
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		for (int i = 0; i < MAP_HEIGHT; ++i) {
 			for (int j = 0; j < MAP_WIDTH; ++j) {
-				SDL_Rect rect = { j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE };
+				SDL_Rect rect = {j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE};
 				SDL_RenderFillRect(renderer, &rect);
 			}
 		}
