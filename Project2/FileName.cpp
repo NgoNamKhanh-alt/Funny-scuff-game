209217540406574ef0bc42e85ae5d6c3077d9685
NgoNamKhanh-alt <ngonamkhanh2710@gmail.com>
2025-04-04 21:07:13 +0700

    Basic starting menu

--- Project2/FileName.cpp
+++ Project2/FileName.cpp
@@ -5,12 +5,13 @@
 	#include <vector>
 	#include <random>
 	#include <chrono>
+	#include <SDL_mixer.h>
 	const int SCREEN_WIDTH = 1260;
 	const int SCREEN_HEIGHT = 720;
 	const int TILE_SIZE = 36;
 	const int MAP_WIDTH = SCREEN_WIDTH / TILE_SIZE;
 	const int MAP_HEIGHT = SCREEN_HEIGHT / TILE_SIZE;
-	const int SHOT_DELAY = 150;
+	const int SHOT_DELAY = 100;
 	const int ENEMY_SHOT_DELAY = 1500;
 	const int ENEMY_SPEED_DELAY = 2500;
 	const int FODDER_DELAY = 1000;
@@ -51,6 +52,7 @@
 	};
 	class Enemy {
 	public:
+		bool alive = true;
 		int health = 40;
 		int x, y;
 		int y_speed = 1;
@@ -180,9 +182,27 @@
 			SDL_FreeSurface(loadedSurface);
 			return newTexture;
 		}
+		Mix_Chunk* loadSound(const char* path) {
+			Mix_Chunk* sound = Mix_LoadWAV(path);
+			if (!sound) {
+				std::cerr << "Failed to load sound: " << Mix_GetError() << std::endl;
+			}
+			return sound;
+		}
+		Mix_Music* loadMusic(const char* path) {
+			Mix_Music* music = Mix_LoadMUS(path);
+			if (!music) {
+				std::cerr << "Failed to load music: " << Mix_GetError() << std::endl;
+			}
+			return music;
+		}
 		Player player;
 		SDL_Window* window;
 		SDL_Renderer* renderer;
+		Mix_Chunk* fumo_hit = NULL;
+		Mix_Chunk* player_hit = NULL;
+		Mix_Chunk* fodder_hit = NULL;
+		Mix_Music* music = NULL;
 		std::vector<Player_Bullet> bullets;
 		Enemy enemy;
 		std::vector<Enemy_Bullet> enemy_bullets;
@@ -193,9 +213,21 @@
 		SDL_Texture* player_texture = NULL;
 		SDL_Texture* bullet_texture = NULL;
 		SDL_Texture* background_texture = NULL;
+		SDL_Texture* start_button = NULL;
+		SDL_Texture* quit_button = NULL;
+		SDL_Rect start_rect = { SCREEN_WIDTH / 2 - (TILE_SIZE * 2 * 7 / 4) / 2, SCREEN_HEIGHT * 2 / 3,TILE_SIZE * 2 * 7 / 4, TILE_SIZE * 5 / 3 };
+		SDL_Rect quit_rect = { SCREEN_WIDTH / 2 - TILE_SIZE, SCREEN_HEIGHT * 2 / 3 + TILE_SIZE * 5 / 3 + TILE_SIZE / 2, TILE_SIZE * 2, TILE_SIZE * 4 / 3 };
+		bool menu_running;
 		bool running;
 		Game() : enemy(SCREEN_WIDTH / 2.2, SCREEN_HEIGHT / 6) {
-			running = true;
+			menu_running = true;
+			running = false;
+			if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+				std::cerr << "SDL Init Error: " << SDL_GetError() << "\n";
+			}
+			if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096) == -1) {
+				std::cerr << "Mix_OpenAudio Error: " << Mix_GetError() << "\n";
+			}
 			if (SDL_Init(SDL_INIT_VIDEO) < 0)
 			{
 				std::cerr << "SDL could not initialize! SDL Error:" << SDL_GetError() << "\n";
@@ -211,97 +243,134 @@
 				std::cerr << "Renderer could not be created! SDL Error: " << SDL_GetError() << "\n";
 				running = false;
 			}
+			player_hit = loadSound("sound/player_hit.wav");
+			fumo_hit = loadSound("sound/fumo_hit.wav");
+			fodder_hit = loadSound("sound/ghost_hit.wav");
 			enemy_texture = loadTexture("img/cirno_fumo.png");
 			enemy_bullet_texture = loadTexture("img/snowyflake.png");
 			fodder_texture = loadTexture("img/fairy.png");
 			player_texture = loadTexture("img/REI.png");
 			bullet_texture = loadTexture("img/bullet.png");
 			background_texture = loadTexture("img/background.png");
+			start_button = loadTexture("img/start.png");
+			quit_button = loadTexture("img/quit.png");
 			player = Player((((MAP_WIDTH - 1) / 2) * TILE_SIZE), ((MAP_HEIGHT - 2) * TILE_SIZE));
 		}
 
 		void update() {
-			Uint32 currentTime = SDL_GetTicks();
-			if (currentTime - fodder_spawn >= FODDER_DELAY) {
-				fodders.push_back(Fodders(player.x + fodder_plm(mt), 0, player.x));
-				fodders.push_back(Fodders(player.x + fodder_plm(mt) + fodder_dis(mt), 0, player.x));
-				fodders.push_back(Fodders(player.x + fodder_plm(mt) - fodder_dis(mt), 0, player.x));
-				fodder_spawn = currentTime;
-			}
-			for (int i = 0; i < fodders.size(); ++i) {
-				fodders[i].move(player);
-				if (fodders[i].y > 720) {
-					fodders.erase(fodders.begin() + i);
+			
+				Uint32 currentTime = SDL_GetTicks();
+				if (currentTime - fodder_spawn >= FODDER_DELAY) {
+					fodders.push_back(Fodders(player.x + fodder_plm(mt), 0, player.x));
+					fodders.push_back(Fodders(player.x + fodder_plm(mt) + fodder_dis(mt), 0, player.x));
+					fodders.push_back(Fodders(player.x + fodder_plm(mt) - fodder_dis(mt), 0, player.x));
+					fodder_spawn = currentTime;
 				}
-			}
-			for (int i = 0; i < bullets.size(); ++i) {
-				bullets[i].move();
-				if (bullets[i].y < 0) {
-					bullets.erase(bullets.begin() + i);
+				for (int i = 0; i < fodders.size(); ++i) {
+					fodders[i].move(player);
+					if (fodders[i].y > 720) {
+						fodders.erase(fodders.begin() + i);
+					}
 				}
-			}
-			int enemy_center = enemy.x + enemy.rect.w / 2;
-			int player_center = player.x + player.rect.w / 2;
-			if (enemy.y < 20) {
-				enemy.y_speed = 2;
-				last_speed_change = currentTime;
-			}
-			else if (enemy.y > 315) {
-				enemy.y_speed = -2;
-				last_speed_change = currentTime;
-			}
-			else if (currentTime - last_speed_change >= ENEMY_SPEED_DELAY) {
-				enemy.y_speed = speed_enemy(mt);
-				if (enemy.y_speed == 0) {
-					enemy.y_speed = 1;
+				for (int i = 0; i < bullets.size(); ++i) {
+					bullets[i].move();
+					if (bullets[i].y < 0) {
+						bullets.erase(bullets.begin() + i);
+					}
 				}
-				last_speed_change = currentTime;
-			}
-			if (enemy_center > player_center) {
-				enemy.move(-3, enemy.y_speed);
-			}
-			else if (enemy_center < player_center) {
-				enemy.move(3, enemy.y_speed);
-			}
-			Uint32 enemy_currentTime = SDL_GetTicks();
-			if (enemy_currentTime - last_enemy_shot_time >= ENEMY_SHOT_DELAY) {
-				enemy_bullets.push_back(Enemy_Bullet(enemy_center, enemy.y + TILE_SIZE * 3, 0, 1));
-				enemy_bullets.push_back(Enemy_Bullet(enemy_center + TILE_SIZE, enemy.y + TILE_SIZE * 3, 0.75, 0.85));
-				enemy_bullets.push_back(Enemy_Bullet(enemy_center - TILE_SIZE, enemy.y + TILE_SIZE * 3, -0.75, 0.85));
-				enemy_bullets.push_back(Enemy_Bullet(enemy_center - TILE_SIZE * 2, enemy.y + TILE_SIZE, -1, 0));
-				enemy_bullets.push_back(Enemy_Bullet(enemy_center + TILE_SIZE * 2, enemy.y + TILE_SIZE, 1, 0));
-				enemy_bullets.push_back(Enemy_Bullet(enemy_center - TILE_SIZE, enemy.y - TILE_SIZE, -0.75, -0.85));
-				enemy_bullets.push_back(Enemy_Bullet(enemy_center + TILE_SIZE, enemy.y - TILE_SIZE, 0.75, -0.85));
-				enemy_bullets.push_back(Enemy_Bullet(enemy_center, enemy.y - TILE_SIZE, 0, -1));
-				last_enemy_shot_time = enemy_currentTime;
-			}
-			for (int i = 0; i < enemy_bullets.size(); ++i) {
-				enemy_bullets[i].move();
-				if (enemy_bullets[i].y > 720 || enemy_bullets[i].y < 0) {
-					enemy_bullets.erase(enemy_bullets.begin() + i);
+				int enemy_center = enemy.x + enemy.rect.w / 2;
+				int player_center = player.x + player.rect.w / 2;
+				if (enemy.y < 20) {
+					enemy.y_speed = 2;
+					last_speed_change = currentTime;
 				}
-			}
-
+				else if (enemy.y > 315) {
+					enemy.y_speed = -2;
+					last_speed_change = currentTime;
+				}
+				else if (currentTime - last_speed_change >= ENEMY_SPEED_DELAY) {
+					enemy.y_speed = speed_enemy(mt);
+					if (enemy.y_speed == 0) {
+						enemy.y_speed = 1;
+					}
+					last_speed_change = currentTime;
+				}
+				if (enemy_center > player_center) {
+					enemy.move(-3, enemy.y_speed);
+				}
+				else if (enemy_center < player_center) {
+					enemy.move(3, enemy.y_speed);
+				}
+				Uint32 enemy_currentTime = SDL_GetTicks();
+				if (enemy_currentTime - last_enemy_shot_time >= ENEMY_SHOT_DELAY) {
+					enemy_bullets.push_back(Enemy_Bullet(enemy_center, enemy.y + TILE_SIZE * 3, 0, 1));
+					enemy_bullets.push_back(Enemy_Bullet(enemy_center + TILE_SIZE, enemy.y + TILE_SIZE * 3, 0.75, 0.85));
+					enemy_bullets.push_back(Enemy_Bullet(enemy_center - TILE_SIZE, enemy.y + TILE_SIZE * 3, -0.75, 0.85));
+					enemy_bullets.push_back(Enemy_Bullet(enemy_center - TILE_SIZE * 2, enemy.y + TILE_SIZE, -1, 0));
+					enemy_bullets.push_back(Enemy_Bullet(enemy_center + TILE_SIZE * 2, enemy.y + TILE_SIZE, 1, 0));
+					enemy_bullets.push_back(Enemy_Bullet(enemy_center - TILE_SIZE, enemy.y - TILE_SIZE, -0.75, -0.85));
+					enemy_bullets.push_back(Enemy_Bullet(enemy_center + TILE_SIZE, enemy.y - TILE_SIZE, 0.75, -0.85));
+					enemy_bullets.push_back(Enemy_Bullet(enemy_center, enemy.y - TILE_SIZE, 0, -1));
+					last_enemy_shot_time = enemy_currentTime;
+				}
+				for (int i = 0; i < enemy_bullets.size(); ++i) {
+					enemy_bullets[i].move();
+					if (enemy_bullets[i].y > 720 || enemy_bullets[i].y < 0) {
+						enemy_bullets.erase(enemy_bullets.begin() + i);
+					}
+				}
+			
 		}
 		void render() {
-
 			SDL_RenderClear(renderer);
 			SDL_RenderCopy(renderer, background_texture, NULL, NULL);
 			for (int i = 0; i < fodders.size(); ++i) {
-				fodders[i].render(renderer,fodder_texture);
+				fodders[i].render(renderer, fodder_texture);
 			}
-			player.render(renderer,player_texture);
+			player.render(renderer, player_texture);
 			for (int i = 0; i < bullets.size(); ++i) {
-				bullets[i].render(renderer,bullet_texture);
+				bullets[i].render(renderer, bullet_texture);
 			}
 			for (int i = 0; i < enemy_bullets.size(); ++i) {
-				enemy_bullets[i].render(renderer,enemy_bullet_texture);
+				enemy_bullets[i].render(renderer, enemy_bullet_texture);
 			}
-			enemy.render(renderer,enemy_texture);
+			
+				enemy.render(renderer, enemy_texture);
+			
 			SDL_RenderPresent(renderer);
 
 		}
+		void render_menu() {
+			SDL_RenderClear(renderer);
+			SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+			SDL_RenderCopy(renderer, background_texture, NULL, NULL);
+			SDL_RenderCopy(renderer, start_button, NULL, &start_rect);
+			SDL_RenderCopy(renderer, quit_button, NULL, &quit_rect);
+			SDL_RenderPresent(renderer);
+		}
+		void menu() {
+			SDL_Event event;
+			while (menu_running) {
+				render_menu();
+				while (SDL_PollEvent(&event)) {
+					if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT) {
+						SDL_Point mousePoint = { event.button.x, event.button.y };
+						if (SDL_PointInRect(&mousePoint, &start_rect)) {
+							running = true;
+							menu_running = false;
+						}
+						else if (SDL_PointInRect(&mousePoint, &quit_rect)) {
+							menu_running = false;
+							running = false;
+						}
+					}
+				}
+			}
+		}
 		void run() {
+			while (menu_running) {
+				menu();
+			}
 			while (running) {
 				handleEvents();
 				render();
@@ -310,6 +379,7 @@
 			}
 		}
 		void handleEvents() {
+
 			SDL_Event event;
 			const Uint8* keystates = SDL_GetKeyboardState(NULL);
 			if (keystates[SDL_SCANCODE_W]) {
@@ -335,12 +405,16 @@
 			for (int i = fodders.size() - 1; i >= 0; --i) {
 				if (SDL_HasIntersection(&player.rect, &fodders[i].rect)) {
 					player.health -= 1;
+					int channel = Mix_PlayChannel(-1, player_hit, 0);
+					Mix_Volume(channel, 64);
 					fodders.erase(fodders.begin() + i);
 				}
 			}
 			for (int i = enemy_bullets.size() - 1; i >= 0; --i) {
 				if (SDL_HasIntersection(&player.rect, &enemy_bullets[i].rect)) {
 					player.health -= enemy_bullets[i].damage;
+					int channel = Mix_PlayChannel(-1, player_hit, 0);
+					Mix_Volume(channel, 64);
 					enemy_bullets.erase(enemy_bullets.begin() + i);
 				}
 			}
@@ -350,6 +424,8 @@
 						fodders[j].health -= bullets[i].damage;
 						bullets.erase(bullets.begin() + i);
 						if (fodders[j].health <= 0) {
+							int channel = Mix_PlayChannel(-1, fodder_hit, 0);
+							Mix_Volume(channel, 9);
 							fodders.erase(fodders.begin() + j);
 						}
 						break;
@@ -359,9 +435,11 @@
 			for (int i = bullets.size() - 1; i >= 0; i--) {
 				if (SDL_HasIntersection(&bullets[i].rect, &enemy.rect)) {
 					enemy.health -= bullets[i].damage;
+					int channel = Mix_PlayChannel(-1, fumo_hit, 0);
+					Mix_Volume(channel, 15);
 					bullets.erase(bullets.begin() + i);
 					if (enemy.health <= 0) {
-						running = false;
+						enemy.alive = false;
 					}
 				}
 			}
@@ -370,7 +448,7 @@
 					running = false;
 				}
 			}
-		}
+	}
 		~Game() {
 			SDL_DestroyRenderer(renderer);
 			SDL_DestroyWindow(window);
@@ -380,7 +458,7 @@
 
 	int main(int argc, char** argv) {
 		Game game;
-		if (game.running) {
+		if (game.running || game.menu_running) {
 			game.run();
 			SDL_Delay(16);
 		}
